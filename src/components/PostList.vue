ed8bfaffb42b9cf1db6a9ca0f9e04ed4a13d0778
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-02-03 18:01:30 +0200

    feat: animations

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,19 +1,20 @@
 <template>
-  <ul class="posts-list">
-    <li v-for="post in posts" :key="post.id" v-if="posts.length > 0">
-      <PostItem :post="post" @remove="$emit('remove', post)" />
-    </li>
-    <li v-else>
-      <p class="posts__hint">Постов нет =(</p>
-    </li>
+  <ul class="posts-list" v-if="posts.length > 0">
+    <TransitionGroup name="post-list">
+      <li v-for="post in posts" :key="post.id">
+        <PostItem :post="post" @remove="$emit('remove', post)" />
+      </li>
+    </TransitionGroup>
   </ul>
+  <p v-else class="posts__hint">Постов нет =(</p>
 </template>
 
 <script>
+import { TransitionGroup } from 'vue';
 import PostItem from './PostItem.vue';
 
 export default {
-  components: { PostItem },
+  components: { PostItem, TransitionGroup },
   props: {
     posts: {
       type: Array,
@@ -33,5 +34,25 @@ export default {
 }
 .posts__hint {
   color: green;
+}
+
+/* animation */
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.post-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
